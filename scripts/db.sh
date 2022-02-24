abdf1d5d8c064f0278ca973d6459094f8c97b87f
Chuying He <heidihcy@outlook.com>
2022-02-24 21:45:34 +0100

    correct the logic in db.sh

--- scripts/db.sh
+++ scripts/db.sh
@@ -3,7 +3,7 @@ POSTGRES_CONTAINER="ap-postgres-container"
 
 docker pull postgres
 
-if [[ $(docker ps -a --filter="name=$POSTGRES_CONTAINER") ]]
+if [[ $(docker ps -a --filter="name=$POSTGRES_CONTAINER") == "" ]]
 then
   echo "> Creating container $POSTGRES_CONTAINER ..."
   docker run --name $POSTGRES_CONTAINER \
@@ -13,12 +13,12 @@ then
     -p 5432:5432 \
     -d postgres
 else
-  if [[ -z $(docker ps --filter="name=$POSTGRES_CONTAINER") ]]
+  if [[ $(docker ps --filter="name=$POSTGRES_CONTAINER") == "" ]]
   then
-    echo "> Container $POSTGRES_CONTAINER is already running"
-  else
     echo "> Restarting existing $POSTGRES_CONTAINER"
     docker restart $POSTGRES_CONTAINER
+  else
+    echo "> Container $POSTGRES_CONTAINER is already running"
   fi
 fi
 
